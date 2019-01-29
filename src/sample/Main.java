c14111c5d8f90744d97b48b2dc711263c50173f0
IBracharz <Ivo.bracharz@gmail.com>
2019-01-29 11:48:20 +0100

    First commit via IntelliJ

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,46 +11,91 @@ package sample;
 import environment.Board;
 import environment.Cell;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+// import javafx.animation.Timeline; -->used to update a nodes properties, such as color, size etc. only need to update the centerPane(TilePane) with this.
+
 
 public class Main extends Application {
 
+    private Stage primaryStage;
+    Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
 
-        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+        //show the main window
+        mainWindow();
 
-        // get the controller
-        Controller controller = new Controller();
+        controller.getTilepane().setPrefTileWidth(10);
+        controller.getTilepane().setPrefTileHeight(10);
 
-        //declare and initialize board
-        Board board = new Board(100,100);
+        System.out.println(controller.getTilepane().getWidth());
+        System.out.println(controller.getTilepane().getHeight());
+
+
+
+
+        //fill the board
+        fillBoard();
+
+    }
+
+    public void fillBoard(){
+        int a = (int) controller.getTilepane().getHeight()/10;
+        int b = (int) controller.getTilepane().getWidth()/10;
+        System.out.println(a+" "+b);
+
+        Board board = new Board(a,b);
+
+
 
         for(int i = 0; i < board.getBoard().length; i++) {
             for(int j = 0; j < board.getBoard()[i].length; j++) {
-                controller.getCenterPane().getChildren().add(board.getCell(i,j).getRec());
+                controller.getTilepane().getChildren().add(board.getCell(i,j).getRec());
             }
         }
-
-        //System.out.println(board.getCell(1,1).getRec());
-
-        //
-
-        //Build new scene from root node.
-        primaryStage.setScene(new Scene(root,800,600));
-        primaryStage.setTitle("Simulation");
-        primaryStage.show();
     }
+
+
+    public void mainWindow(){
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+            Parent root = loader.load();
+
+            controller = loader.getController();
+            controller.setMain(this);
+
+            primaryStage.setMinWidth(850);
+            primaryStage.setMinHeight(650);
+
+
+            Scene myScene = new Scene(root);
+            primaryStage.setScene(myScene);
+            primaryStage.setTitle("Simulation");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     //starting point of application
     public static void main(String[] args) {
-
     launch(args);
-
     }
 }
 
